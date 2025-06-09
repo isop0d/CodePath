707ac1763eddfc4cb58bbecd10691fc7a99bbeb8
Stefan Fernandez <stefanfernandez2005@gmail.com>
2025-06-09 15:05:03 -0400

    init

---
+++ CodePath Labs/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for Stefan</h1>
+      <h2>Stefan... Lock in</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
