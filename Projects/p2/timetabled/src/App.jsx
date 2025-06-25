b0526cd15aea32e244640202716443ebc447dca2
Stefan Fernandez <stefanfernandez2005@gmail.com>
2025-06-24 21:01:23 -0400

    change made to update submit feature

--- Projects/p2/timetabled/src/App.jsx
+++ Projects/p2/timetabled/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { authorList } from "./assets/data";
 
 function App() {
   const [currentnum, setCurrentnum] = useState(0);
   const [flipped, setFlipped] = useState(false);
+  const [userGuess, setUserGuess] = useState(" ");
 
   let author = authorList[currentnum];
 
@@ -14,10 +13,28 @@ function App() {
     setFlipped(false);
     setCurrentnum((prev) => {
       let next = prev + direction;
+      // if (next < 0) {
+      // maybe add something here to hide previous button
+      // }
       if (next < 0 || next >= authorList.length) return prev;
       return next;
     });
   };
+
+  const handleInput = (e) => setUserGuess(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(userGuess.trim().toLowerCase() === author.author.toLowerCase()) {  
+    alert("Correct!");
+    } else {
+    alert("Incorrect!");
+    }
+    setUserGuess("")
+  };
+
+  // return <input type='text' value={userGuess} onChange={handleInput}/> use this later
+
   return (
     <div className="core">
       <h2>Guess who said it!</h2>
@@ -26,8 +43,18 @@ function App() {
       <div className="card" onClick={() => setFlipped(!flipped)}>
         <h3>{flipped ? author.author : `"${author.quote}"`}</h3>
       </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input bar"
+          type="text"
+          value={userGuess}
+          onChange={(e) => setUserGuess(e.target.value)}
+          placeholder="here"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
       <button onClick={() => handleclick(-1)}>Previous</button>
-      <button onClick={() => handleclick(+1)}>Next</button>
+      <button onClick={() => handleclick(+1)}>Next</button> 
     </div>
   );
 }
