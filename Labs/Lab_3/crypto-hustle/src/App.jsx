324d57fcef2b3179fda2b8b8f935d41a3ed63977
Stefan Fernandez <stefanfernandez2005@gmail.com>
2025-07-11 16:39:06 -0400

    init

---
+++ Labs/Lab_3/crypto-hustle/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+const API_KEY = import.meta.env.VITE_APP
+
+
+function App() {
+  const [list, setList] = useState(null)
+  useEffect(() => {
+  
+    fetchAllCoinData(){
+      fetch
+    }
+
+  }, [])
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
---
+++ Projects/p3/project-5/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Discover Dogs!</h2>
+      <h3>click the button to start</h3>
+      <button></button>
+    </div>
+  )
+}
+
+export default App
