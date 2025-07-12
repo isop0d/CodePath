e662a90131c70b3b379edbe856ace29a355f2e1e
Stefan Fernandez <stefanfernandez2005@gmail.com>
2025-07-12 12:39:14 -0400

    init 2

--- Projects/p3/project-5/src/App.jsx
+++ Projects/p3/project-5/src/App.jsx
@@ -2,13 +2,41 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dogData, setDogData] = useState(null)
+
+  const parseBreedFromUrl = (url) => {
+    const match = url.match(/breeds\/([^\/]+)\//);
+    if (!match) return{ breed: 'Uknown', subBreed: 'None'}
+
+    const parts = match[1].split('-');
+    return{
+      breed: parts[0],
+      subBreed: parts[1],
+    };
+  };
+
+  const fetchDog = async () => {
+    let dog;
+    let parsed;
+    
+    
+    const result = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await result.json();
+    const imageUrl = data.message;
+    
+    parsed = parseBreedFromUrl(imageUrl);
+    dog = {...parsed, image: imageUrl};
+    
+    
+    setDogData(dog)
+    return dog
+  }
 
   return (
     <div>
       <h2>Discover Dogs!</h2>
       <h3>click the button to start</h3>
-      <button></button>
+      <button onClick={fetchDog()}></button>
     </div>
   )
 }
